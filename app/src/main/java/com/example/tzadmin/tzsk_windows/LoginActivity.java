0ed86e8d74855119118fb4874627b5dbb89420a6
barbeu <yuriy.barbeu@gmail.com>
2017-04-07 13:09:02 +0300

    Added activity login.

--- app/src/main/java/com/example/tzadmin/tzsk_windows/LoginActivity.java
+++ app/src/main/java/com/example/tzadmin/tzsk_windows/LoginActivity.java
@@ -1,16 +1,13 @@
 package com.example.tzadmin.tzsk_windows;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        setContentView(R.layout.activity_login);
     }
 }
