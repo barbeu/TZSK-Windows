1e2c1252ba44a199203b59465933d73bf5913384
barbeu <yuriy.barbeu@gmail.com>
2017-04-11 11:40:53 +0300

    Fixed http module, added struct HttpResp.

--- app/src/main/java/com/example/tzadmin/tzsk_windows/HttpModule/Http.java
+++ app/src/main/java/com/example/tzadmin/tzsk_windows/HttpModule/Http.java
@@ -2,7 +2,6 @@ package com.example.tzadmin.tzsk_windows.HttpModule;
 
 import android.os.AsyncTask;
 import android.util.Base64;
-
 import com.example.tzadmin.tzsk_windows.SaveAuthModule.SaveAuth;
 import com.example.tzadmin.tzsk_windows.helper;
 import java.io.BufferedWriter;
@@ -18,47 +17,65 @@ import java.util.concurrent.ExecutionException;
  */
 
 public class Http {
-    String responce = null, data = null;
+    String query = null, data = null;
+    HttpResp responce = null;
 
-    public String GET (String inquiry) throws ExecutionException, InterruptedException {
+    public HttpResp GET (String inquiry) {
+        query = inquiry;
         getTask task = new getTask();
-        task.execute(inquiry);
-        return task.get();
+        task.execute();
+        try {
+            return task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public String POST (String inquiry, String postData) throws ExecutionException, InterruptedException {
+    public HttpResp POST (String inquiry, String postData) {
+        query = inquiry;
         data = postData;
         postTask task = new postTask();
-        task.execute(inquiry);
-        return task.get();
+        task.execute();
+        try {
+            return task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    class getTask extends AsyncTask<String, Void, String> {
+    class getTask extends AsyncTask<Void, Void, HttpResp> {
         @Override
-        protected String doInBackground(String... inquiry) {
+        protected HttpResp doInBackground(Void... params) {
             try {
-                URL url = new URL(helper.httpServer + inquiry);
+                URL url = new URL(helper.httpServer + query);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestProperty("Authorization", "Basic " + Base64.encodeToString((SaveAuth.login + ":" + SaveAuth.psswd).getBytes(), Base64.NO_WRAP ));
                 connection.connect();
-                int code = connection.getResponseCode();
-                return connection.getResponseMessage();
-
+                responce = new HttpResp();
+                responce.Code = connection.getResponseCode();
+                responce.Message = connection.getResponseMessage();
+                return responce;
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
             }
         }
-        protected void onPostExecute (String result) {
+        protected void onPostExecute (HttpResp result) {
             responce = result;
         }
     }
 
-    class postTask extends AsyncTask<String, Void, String> {
+    class postTask extends AsyncTask<Void, Void, HttpResp> {
         @Override
-        protected String doInBackground(String... inquiry) {
+        protected HttpResp doInBackground(Void... inquiry) {
             try {
-                URL url = new URL(helper.httpServer + inquiry);
+                URL url = new URL(helper.httpServer + query);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestProperty("Authorization", "Basic " + Base64.encodeToString((SaveAuth.login + ":" + SaveAuth.psswd).getBytes(), Base64.NO_WRAP ));
                 connection.setReadTimeout(10000);
@@ -75,15 +92,17 @@ public class Http {
                 os.close();
 
                 connection.connect();
-                int code = connection.getResponseCode();
-                return connection.getResponseMessage();
+                responce = new HttpResp();
+                responce.Code = connection.getResponseCode();
+                responce.Message = connection.getResponseMessage();
+                return responce;
 
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
             }
         }
-        protected void onPostExecute (String result) {
+        protected void onPostExecute (HttpResp result) {
             responce = result;
         }
     }
---
+++ app/src/main/java/com/example/tzadmin/tzsk_windows/HttpModule/HttpResp.java
@@ -0,0 +1,10 @@
+package com.example.tzadmin.tzsk_windows.HttpModule;
+
+/**
+ * Created by tzadmin on 11.04.17.
+ */
+
+public class HttpResp {
+    int Code;
+    String Message;
+}
