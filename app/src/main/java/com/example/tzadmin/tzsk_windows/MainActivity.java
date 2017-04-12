5b9e8e458af49c94678de69671c755d0621f8260
barbeu <yuriy.barbeu@gmail.com>
2017-04-12 11:27:33 +0300

    Added button exit.

--- app/src/main/java/com/example/tzadmin/tzsk_windows/MainActivity.java
+++ app/src/main/java/com/example/tzadmin/tzsk_windows/MainActivity.java
@@ -1,12 +1,31 @@
 package com.example.tzadmin.tzsk_windows;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.tzadmin.tzsk_windows.SaveAuthModule.SaveAuth;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SaveAuth.SetUp(this);
+    }
+
+    public void onClick (View view) {
+        switch (view.getId()) {
+            case R.id.btn_exit:
+                exit();
+                break;
+        }
+    }
+
+    public void exit() {
+        SaveAuth.clear();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
