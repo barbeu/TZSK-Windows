5a8f9cb2ffa595b8ea95c15df25c930d58266276
barbeu <yuriy.barbeu@gmail.com>
2017-04-13 09:09:51 +0300

    Added method checking internet connection.

--- app/src/main/java/com/example/tzadmin/tzsk_windows/MainActivity.java
+++ app/src/main/java/com/example/tzadmin/tzsk_windows/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 import com.example.tzadmin.tzsk_windows.SaveAuthModule.SaveAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        test t = new test();
     }
 
     private void init() {
--- app/src/main/java/com/example/tzadmin/tzsk_windows/helper.java
+++ app/src/main/java/com/example/tzadmin/tzsk_windows/helper.java
@@ -1,6 +1,8 @@
 package com.example.tzadmin.tzsk_windows;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.widget.Toast;
 
 /**
@@ -33,6 +35,22 @@ public class helper {
         INCORRECT_AUTH_DATA
     }
 
-
+    public static boolean hasConnection(final Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getActiveNetworkInfo();
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
 
 }
